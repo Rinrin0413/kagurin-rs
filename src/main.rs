fc6140d5c8ef2f7a329c8011d201d6920b99d70d
Rinrin0413 <qqt45fhd@ab.auone-net.jp>
2022-01-31 16:45:03 +0900

    Update:v0.1.1

--- src/main.rs
+++ src/main.rs
@@ -6,9 +6,6 @@ use serenity::framework::standard::{
 };
 use serenity::model::channel::Message;
 use std::env;
-use std::fs::File;
-use std::io::ErrorKind;
-use std::io::Read;
 
 #[group]
 #[commands(ping)]
@@ -22,13 +19,11 @@ impl EventHandler for Handler {}
 #[tokio::main]
 async fn main() {
     let framework = StandardFramework::new()
-        .configure(|c| c.prefix("~")) // set the bot's prefix to "~"
+        .configure(|c| c.prefix("kgrs!")) // cmd prefix
         .group(&GENERAL_GROUP);
 
-    // Enter the path to the file containing the bot token (ex: TOKEN)
-    // If the corresponding file is not found in the corresponding path,
-    // it will be created in that path...
-    let token = env::var(open_file("TOKEN")).expect("token");
+    // Login with a bot token from the environment
+    let token = env::var("KAGURIN_RS_TOKEN").expect("token");
     let mut client = Client::builder(token)
         .event_handler(Handler)
         .framework(framework)
@@ -44,22 +39,5 @@ async fn main() {
 #[command]
 async fn ping(ctx: &Context, msg: &Message) -> CommandResult {
     msg.reply(ctx, "Pong!").await?;
-
     Ok(())
-}
-
-fn open_file(path: &str) -> String {
-    let mut fh = match File::open(path) {
-        Err(ref error) if error.kind() == ErrorKind::NotFound => {
-            match File::create("./TOKEN") {
-                Ok(file) => panic!("ファイル `{}` を作成しました。\nそのファイルに bot のトークンを記入してください。\nハンドル: {:?}", path, file),
-                Err(err) =>  panic!("ファイル `{}` の作成を試みましたが問題が発生: {:?}", path, err),
-            }
-        },
-        Err(err) => panic!("ファイルを開く時に問題が発生: {:?}", err),
-        Ok(file) => file,
-    };
-    let mut tkn = String::new();
-    let _ = fh.read_to_string(&mut tkn);
-    tkn.to_string()
 }
