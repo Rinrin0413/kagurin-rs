32555c0001f5018bfec04703c05b370432ef44c4
Rinrin0413 <qqt45fhd@ab.auone-net.jp>
2022-02-09 05:24:45 +0900

    Update:v0.2.0

--- src/main.rs
+++ src/main.rs
@@ -10,15 +10,20 @@ struct Handler;
 
 #[async_trait]
 impl EventHandler for Handler {
-    // メッセージイベント
+    // MSG event
     async fn message(&self, ctx: Context, msg: Message) {
         if msg.content == "kgrs!ping" {
             if let Err(why) = msg.channel_id.say(&ctx.http, "贵樣!").await {
-                println!("Error sending message: {:?}", why);
+                er(why);
+            }
+        }
+        if msg.content == "kgrs!ping2" {
+            if let Err(why) = msg.channel_id.say(&ctx.http, "贵樣！!").await {
+                er(why);
             }
         }
     }
-    // 起動イベント
+    // READY event
     async fn ready(&self, _: Context, ready: Ready) {
         println!("{} is connected", ready.user.name);
     }
@@ -29,7 +34,7 @@ async fn main() {
     // Configure the client with your Discord bot token in the environment.
     let token = env::var("KAGURIN_RS_TOKEN").expect("Expected a token in the environment");
 
-    // Create a new instance of the Client, logging in as a bot. 
+    // Create a new instance of the Client, logging in as a bot.
     let mut client = Client::builder(&token)
         .event_handler(Handler)
         .await
@@ -39,4 +44,8 @@ async fn main() {
     if let Err(why) = client.start().await {
         println!("Client error: {:?}", why);
     }
+}
+
+fn er(why: serenity::Error) {
+    println!("Error sending message: {:?}", why);
 }
