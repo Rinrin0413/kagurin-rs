be923de0ee4a19d1d2812d7e7bfb2e39c05a2de7
Rinrin0413 <qqt45fhd@ab.auone-net.jp>
2022-02-16 09:00:26 +0900

    Add:fmt module(put some functions together in a this mod) Update:integrated is_trusted and is_developer fn to restrict_users fn

--- kgrs/src/util.rs
+++ kgrs/src/util.rs
@@ -1,54 +1,53 @@
-use serenity::model::prelude::{Message, UserId};
-use std::fmt::Debug;
-//use serenity::self;
+use serenity::model::prelude::UserId;
 
-pub enum Et<'a, T> {
-    Rslt(Result<Message, T>),
-    Optn(Option<T>, &'a Message),
-    Other(T),
+pub fn restrict_users(author: UserId, auth_user: &[u64]) -> bool {
+    auth_user.contains(&author.as_u64())
 }
 
-impl<T> Et<'_, T> 
-where 
-    T: ToString + Debug,
-{
-    pub fn l(&self, cmd: &str, at: &str) -> String {
-        match self {
-            Et::Rslt(v) => {
-                if let Err(why) = v {
-                    format!("kgrs!{} / {} : {:?}", cmd, at, why)
-                } else {
-                    format!("kgrs!{} / {} : success", cmd, at)
-                }
-            },
-            Et::Optn(v, ref msg) => {
-                if let Some(val) = v {
-                    val.to_string()
-                } else {
-                    msg.author.name.clone()
-                }
-            },
-            Et::Other(_) => format!("kgrs!{} / {}", cmd, at),
+pub mod fmt {
+    use serenity::model::prelude::Message;
+    use std::fmt::Debug;
+    pub enum Et<'a, T> {
+        Rslt(Result<Message, T>),
+        Optn(Option<T>, &'a Message),
+        Other(T),
+    }
+    
+    impl<T> Et<'_, T> 
+    where 
+        T: ToString + Debug,
+    {
+        pub fn l(&self, cmd: &str, at: &str) -> String {
+            match self {
+                Et::Rslt(v) => {
+                    if let Err(why) = v {
+                        format!("kgrs!{} / {} : {:?}", cmd, at, why)
+                    } else {
+                        format!("kgrs!{} / {} : success", cmd, at)
+                    }
+                },
+                Et::Optn(v, ref msg) => {
+                    if let Some(val) = v {
+                        val.to_string()
+                    } else {
+                        msg.author.name.clone()
+                    }
+                },
+                Et::Other(_) => format!("kgrs!{} / {}", cmd, at),
+            }
         }
     }
-}
+    
+    pub fn want_arg(n: usize) -> String {
+        format!("Error: 最低{}つの引数が必要です", n)
+    }
 
-pub fn want_arg(n: usize) -> String {
-    format!("Error: 最低{}つの引数が必要です", n)
-}
+    pub fn cb(content: &str, lang: &str) -> String {
+        format!("```{}\n{}\n```", lang, content)
+    }
+    
+    pub fn invalid_arg(false_arg: &str, true_args: &str) -> String {
+        format!("無効な引数`{}`を確認\n利用可能な引数: `{}`", false_arg, true_args)
+    }
 
-pub fn is_trusted(author: UserId, trusted: &[u64]) -> bool {
-    trusted.contains(author.as_u64())
-}
-
-pub fn is_developer(author: UserId, developer: [&'static u64; 2]) -> bool {
-    developer.contains(&author.as_u64())
-}
-
-pub fn cb(content: &str, lang: &str) -> String {
-    format!("```{}\n{}\n```", lang, content)
-}
-
-pub fn invalid_arg(false_arg: &str, true_args: &str) -> String {
-    format!("無効な引数`{}`を確認\n利用可能な引数: `{}`", false_arg, true_args)
 }
