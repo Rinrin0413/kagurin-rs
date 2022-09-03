dd540cb2e24a3a0aed575d2c77f9d307bb67af89
Rinrin.rs <rinrin0413.valley@gmail.com>
2022-09-04 00:13:19 +0900

    Update:kgrs v1.1.0-unstable Impl:feature that can use multiple interactions

--- kgrs/src/response_interactions.rs
+++ kgrs/src/response_interactions.rs
@@ -1,20 +1,33 @@
-use serenity::builder::CreateEmbed;
+use serenity::builder::{CreateEmbed, CreateButton};
+
+/// Some interactions.
+/// 
+/// # Variables
+/// 
+/// - `Some`: Some interactions.
+/// - `Dev`: Send "Not implemented yet :<".
+/// - `None`: Do nothing.
+pub enum Interactions {
+    /// Do some interaction with the response.
+    Some(Vec<InteractMode>),
+    /// Send "Not implemented yet :<".
+    Dev,
+    /// Do nothing.
+    None,
+}
 
 /// The interaction mode.
 /// 
 /// # Variants
 /// 
-/// - `Message`: Send a message.
-/// - `Embed`: Send a embed.
-/// - `Dev`: Send "Not implemented yet :<".
-/// - `None`: Do nothing.
+/// - `Message`: Add a message.
+/// - `Embed`: Add a embed.
+/// - `Button`: Add a button.
 pub enum InteractMode {
-    /// Send a message.
+    /// Add a message.
     Message(String),
-    /// Send a embed.
+    /// Add a embed.
     Embed(CreateEmbed),
-    /// Send "Not implemented yet :<".
-    Dev,
-    /// Do nothing.
-    None,
+    /// Add a button.
+    Button(CreateButton),
 }
