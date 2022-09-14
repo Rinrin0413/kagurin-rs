45bb1d75fda6b17d2ce392b28f9e2a67b11f6587
Rinrin.rs <rinrin0413.valley@gmail.com>
2022-09-15 03:03:29 +0900

    Update:kgrs v1.4.0 Add:function `get_uptime` Improve:rustfmt

--- kgrs/src/util.rs
+++ kgrs/src/util.rs
@@ -2,7 +2,7 @@
 
 use std::{process, env};
 
-/// Get the current memory usage by this bot (MiB)
+/// Get the current memory usage by this bot. (MiB)
 pub fn get_memory_usage() -> f64 {
     let stdout = process::Command::new("sh")
         .args([
@@ -31,6 +31,19 @@ pub fn get_memory_usage() -> f64 {
 pub fn round_mid(num: f64, decimal_places: u32) -> f64 {
     let n = 10_u64.pow(decimal_places) as f64;
     (num * n).round() / n
+}
+
+/// Get the current uptime of this bot.
+pub fn get_uptime() -> String {
+    let stdout = process::Command::new("sh")
+        .args([
+            format!("{}/../assets/shells/ps.sh", env!("CARGO_MANIFEST_DIR")),
+            process::id().to_string(),
+        ])
+        .output()
+        .unwrap()
+        .stdout;
+    String::from_utf8_lossy(&stdout).to_string().replace('\n', "")
 }
 
 pub mod fmt {
