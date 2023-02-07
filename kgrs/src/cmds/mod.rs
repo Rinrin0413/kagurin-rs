cb9543786320c78449d1b2828152e286a0549998
Rinrin.rs <rinrin0413.valley@gmail.com>
2023-02-07 09:56:19 +0900

    Update:kgrs v1.10.0 Update:rogger Create:a new module `sfinder`

--- kgrs/src/cmds/mod.rs
+++ kgrs/src/cmds/mod.rs
@@ -2,4 +2,5 @@
 
 pub mod jsd;
 pub mod tetr;
+pub mod sfinder;
 //pub mod playground;
---
+++ kgrs/src/cmds/sfinder.rs
@@ -0,0 +1,16 @@
+//! Functions for solution-finder
+
+use std::{path::Path, fs};
+
+/// Returns a path to the output directory for the solution-finder.
+pub fn output_dir() -> String {
+    format!("{}/../output", env!("CARGO_MANIFEST_DIR"))
+}
+
+/// Initializes the output directory for the solution-finder.
+pub fn init_output_dir() {
+    let output_dir = output_dir();
+    if Path::new(&output_dir).exists() {
+        fs::remove_dir_all(&output_dir).unwrap();
+    }
+}
