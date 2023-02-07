2d263777521c889ad2a7e7990c60534b6ca84cf8
Rinrin.rs <rinrin0413.valley@gmail.com>
2023-02-07 23:10:37 +0900

    Update:kgrs v1.10.1 Fix:not getting the output directory correctly

--- kgrs/src/cmds/sfinder.rs
+++ kgrs/src/cmds/sfinder.rs
@@ -1,10 +1,13 @@
 //! Functions for solution-finder
 
-use std::{fs, path::Path};
+use std::{fs, path::Path, process};
 
 /// Returns a path to the output directory for the solution-finder.
 pub fn output_dir() -> String {
-    format!("{}/../output", env!("CARGO_MANIFEST_DIR"))
+    format!(
+        "{}/output",
+        String::from_utf8_lossy(&process::Command::new("pwd").output().unwrap().stdout).trim()
+    )
 }
 
 /// Initializes the output directory for the solution-finder.
